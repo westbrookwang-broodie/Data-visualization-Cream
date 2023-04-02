1045d760c152256b2a2da4d86092185d42b4d8ab
wang <markwangyu@126,com>
2023-04-02 20:30:25 +0800

    first commit

---
+++ src/pages/my-page/index.vue
@@ -0,0 +1,24 @@
+<template>
+<!--  <div class="login-wrapper">-->
+<!--    <login-header />-->
+<!--    <div class="login-container">-->
+      <p>My Page</p>
+      <h1>yuyuyuyuy</h1>
+<!--      </div>-->
+
+<!--      <tdesign-setting />-->
+
+<!--    <footer class="copyright">Copyright @ 2021-2022 Tencent. All Rights Reserved</footer>-->
+<!--  </div>-->
+</template>
+<script lang="ts">
+export default {
+  name: 'index',
+};
+</script>
+<script setup lang="ts">
+import { ref } from 'vue';
+</script>
+
+<style>
+</style>
