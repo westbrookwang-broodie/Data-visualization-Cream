403954f009af5f868540603c7046c123e67292a7
K-Gonzalez <120776535+K-Gonzalez1204@users.noreply.github.com>
2023-04-23 19:54:33 +0800

    爬取短评脚本完善
    一些更新

--- data/comments/com_short_spider.py
+++ data/comments/com_short_spider.py
@@ -7,7 +7,7 @@ import random
 import re
 
 
-# Python爬虫 爬取电影《你的名字》影片短评
+# Python爬虫 爬取电影影片短评
 class Spider:
 
     # 一些初始化
@@ -16,14 +16,27 @@ class Spider:
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
             "Cookie": "ll=\"118282\"; bid=2FWA5hmFNE0; __gads=ID=b7838384056e8a41-221e0a9df0dc0015:T=1680440281:RT=1680440281:S=ALNI_MZ3Jn-AU5gmKvU0C1hKLIIODDaJmg; ct=y; __utmc=30149280; ap_v=0,6.0; __utma=30149280.915297433.1679663654.1681030236.1681054094.7; __utmb=30149280.0.10.1681054094; __utmz=30149280.1681054094.7.4.utmcsr=blog.csdn.net|utmccn=(referral)|utmcmd=referral|utmcct=/qq_44988175/article/details/127096889; __gpi=UID=00000bea5cf5b656:T=1680440281:RT=1681054153:S=ALNI_MafT6K0MFQTbHWnX16Y5WOqoqYjTQ; dbcl2=\"269613122:7WY2e3vQ9ac\"; ck=xi1N; frodotk_db=\"1aed5ac94973d37d50c8105bdc5b7a77\"; push_noty_num=0; push_doumail_num=0"
         }
-        self.url = "https://movie.douban.com/subject/26683290/comments?start={}&limit=20&status=P&sort=new_score"
-        self.filepath = "../data/test/test.csv"
+        self.url = "https://movie.douban.com/subject/{}/comments?start={}&limit=20&status=P&sort=new_score"
+        self.filepath = "comments/{}/{}.csv"
+
+    # 获取电影id和title列表,从两个整合过后的csv文件里获取
+    def get_id_list(self, path):
+        id_list = []
+        with open(path, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+            for line in lines[1:]:
+                film_info = line.split(',')
+                film_id = film_info[-1][:-1]
+                film_title = film_info[0]
+                id_list.append((film_id, film_title))
+        # print(id_list)
+        return id_list
 
     # 构造url_list
-    def make_url_list(self):
+    def make_url_list(self, film_id):
         url_list = []
         for i in range(30):
-            url_list.append(self.url.format(i * 20))
+            url_list.append(self.url.format(film_id, i * 20))
         return url_list
 
     # 获取单页短评内容，交由后续解析
@@ -40,67 +53,70 @@ class Spider:
 
     def run(self):
         print("-----Spider begin-----")
+        path_list = ["most_pop_film", "most_score_film"]
         start_time = time.time()
-        url_list = self.make_url_list()
-        t = 0
+        for ts in range(2):
+            id_list = self.get_id_list(path_list[ts]+'.csv')
+            for film_info in id_list:
+                url_list = self.make_url_list(film_info[0])
+                t = 0
 
-        movie_id = []
-        movie_title = []
-        movie_comments_user = []  # 用户名
-        movie_comments_time = []  # 发布时间
-        movie_comments_vote = []  # 被点赞数
-        movie_comments_text = []  # 评论内容
+                movie_id = []
+                movie_title = []
+                movie_comments_user = []  # 用户名
+                movie_comments_time = []  # 发布时间
+                movie_comments_vote = []  # 被点赞数
+                movie_comments_text = []  # 评论内容
 
-        for url in url_list:
-            page = self.res_decode(url)
+                for url in url_list:
+                    page = self.res_decode(url)
 
-            for i in range(20):
-                movie_id.append("26683290")
-                movie_title.append("你的名字。")
+                    for i in range(20):
+                        movie_id.append(film_info[0])
+                        movie_title.append(film_info[1])
 
-            # 被点赞数
-            vote = page.find_all(class_="votes vote-count")
-            vote = re.findall('votes vote-count">(.*?)</span>', str(vote))
-            movie_comments_vote = self.data_concat(movie_comments_vote, vote)
+                    # 被点赞数
+                    vote = page.find_all(class_="votes vote-count")
+                    vote = re.findall('votes vote-count">(.*?)</span>', str(vote))
+                    movie_comments_vote = self.data_concat(movie_comments_vote, vote)
 
-            info = page.find_all(class_='comment-info')
-            # 用户名
-            user = re.findall('href.*?/">(.*?)</a>', str(info))
-            movie_comments_user = self.data_concat(movie_comments_user, user)
+                    info = page.find_all(class_='comment-info')
+                    # 用户名
+                    user = re.findall('href.*?/">(.*?)</a>', str(info))
+                    movie_comments_user = self.data_concat(movie_comments_user, user)
 
-            # 发布时间
-            c_time = page.find_all(class_='comment-time')
-            c_time = re.findall('title="(.*?)"', str(c_time))
-            movie_comments_time = self.data_concat(movie_comments_time, c_time)
+                    # 发布时间
+                    c_time = page.find_all(class_='comment-time')
+                    c_time = re.findall('title="(.*?)"', str(c_time))
+                    movie_comments_time = self.data_concat(movie_comments_time, c_time)
 
-            # 评论内容
-            text = page.find_all(class_='short')
-            text = re.findall('"short">((?:.|\n)*?)</span>', str(text))
-            movie_comments_text = self.data_concat(movie_comments_text, text)
+                    # 评论内容
+                    text = page.find_all(class_='short')
+                    text = re.findall('"short">((?:.|\n)*?)</span>', str(text))
+                    movie_comments_text = self.data_concat(movie_comments_text, text)
 
-            print("page %d finish" % t)
-            t += 1
-            time.sleep(random.random() * 5)
+                    print("page %d finish" % t)
+                    t += 1
+                    time.sleep(random.random() * 5)
 
-            print("id: %d" % len(movie_id))
-            print("title: %d" % len(movie_title))
-            print("c user: %d" % len(movie_comments_user))
-            print("c time: %d" % len(movie_comments_time))
-            print("c vote: %d" % len(movie_comments_vote))
-            print("c text: %d" % len(movie_comments_text))
+                    print("id: %d" % len(movie_id))
+                    print("title: %d" % len(movie_title))
+                    print("c user: %d" % len(movie_comments_user))
+                    print("c time: %d" % len(movie_comments_time))
+                    print("c vote: %d" % len(movie_comments_vote))
+                    print("c text: %d" % len(movie_comments_text))
 
-        dataframe = pd.DataFrame({
-            "movie_id": movie_id,
-            "movie_title": movie_title,
-            "comment_user": movie_comments_user,
-            "comment_time": movie_comments_time,
-            "comment_vote": movie_comments_vote,
-            "comment_text": movie_comments_text
-        })
-        dataframe.to_csv(self.filepath, encoding='utf-8', sep='$')
-        end_time = time.time()
-        print("-----Spider finish in %f s-----" % (end_time - start_time))
-        pass
+                dataframe = pd.DataFrame({
+                    "movie_id": movie_id,
+                    "movie_title": movie_title,
+                    "comment_user": movie_comments_user,
+                    "comment_time": movie_comments_time,
+                    "comment_vote": movie_comments_vote,
+                    "comment_text": movie_comments_text
+                })
+                dataframe.to_csv(self.filepath.format(path_list[ts], film_info[1]), encoding='utf-8', sep='$')
+                end_time = time.time()
+                print("-----Spider of film %s finish at %f s-----" % (film_info[0], end_time - start_time))
 
 
 if __name__ == '__main__':
--- data/comments/film_group.py
+++ data/comments/film_group.py
@@ -1,1 +1,46 @@
-import pandas as pd
+# 整合most_pop_film和most_score_film的文件到一个csv文件中,方便后续读取
+pop_path = "most_pop_film/S{}.csv"
+score_path = "most_score_film/高分{}.csv"
+
+pop_list = []
+score_list = []
+for i in range(1, 11):
+    pop_list.append(pop_path.format(str(i)))
+    score_list.append(score_path.format(str(i)))
+
+pop_path = "most_pop_film.csv"
+score_path = "most_score_film.csv"
+
+pop_t = ''
+score_t = ''
+
+flag = 1
+for path in pop_list:
+    with open(path, 'r', encoding='utf-8') as file1:
+        pop = file1.readlines()
+        if flag:
+            pop_t += pop[0]
+            flag = 0
+        for j in range(1, len(pop)):
+            pop_t += pop[j]
+    file1.close()
+
+flag = 1
+for path in score_list:
+    with open(path, 'r', encoding='utf-8') as file2:
+        pop = file2.readlines()
+        if flag:
+            score_t += pop[0]
+            flag = 0
+        for j in range(1, len(pop)):
+            score_t += pop[j]
+    file2.close()
+
+with open(pop_path, 'w', encoding='utf-8') as t:
+    t.write(pop_t)
+
+with open(score_path,'w',encoding='utf-8') as t:
+    t.write(score_t)
+
+
+
