58d679cb2e3971e87431481228376d098c6bd7c7
K-Gonzalez <120776535+K-Gonzalez1204@users.noreply.github.com>
2023-04-23 21:42:41 +0800

    短评爬虫改动
    别运行，这玩意要跑六七个小时

--- data/comments/com_short_spider.py
+++ data/comments/com_short_spider.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 import time
 import random
 import re
+import os
 
 
 # Python爬虫 爬取电影影片短评
@@ -39,6 +40,17 @@ class Spider:
             url_list.append(self.url.format(film_id, i * 20))
         return url_list
 
+    # 检测某电影是否已经爬完整，方便脚本中断重新爬取时跳过以节省时间，0就是没爬完
+    def check_output(self, path):
+        if not os.path.exists(path):
+            return 0
+        with open(path, 'r', encoding='utf-8') as f:
+            lis = f.readlines()
+            if lis[-1].split('$')[0] == '599':
+                return 1
+            else:
+                return 0
+
     # 获取单页短评内容，交由后续解析
     def res_decode(self, url):
         res = requests.get(url=url, headers=self.headers)
@@ -58,9 +70,11 @@ class Spider:
         for ts in range(2):
             id_list = self.get_id_list(path_list[ts]+'.csv')
             for film_info in id_list:
+                output_path = self.filepath.format(path_list[ts], film_info[1])
+                if self.check_output(output_path):
+                    continue
                 url_list = self.make_url_list(film_info[0])
                 t = 0
-
                 movie_id = []
                 movie_title = []
                 movie_comments_user = []  # 用户名
@@ -68,6 +82,7 @@ class Spider:
                 movie_comments_vote = []  # 被点赞数
                 movie_comments_text = []  # 评论内容
 
+                print("Film id: %s" % (film_info[0]))
                 for url in url_list:
                     page = self.res_decode(url)
 
@@ -97,14 +112,14 @@ class Spider:
 
                     print("page %d finish" % t)
                     t += 1
-                    time.sleep(random.random() * 5)
+                    time.sleep(random.random() * 3)
 
-                    print("id: %d" % len(movie_id))
-                    print("title: %d" % len(movie_title))
-                    print("c user: %d" % len(movie_comments_user))
-                    print("c time: %d" % len(movie_comments_time))
-                    print("c vote: %d" % len(movie_comments_vote))
-                    print("c text: %d" % len(movie_comments_text))
+                    # print("id: %d" % len(movie_id))
+                    # print("title: %d" % len(movie_title))
+                    # print("c user: %d" % len(movie_comments_user))
+                    # print("c time: %d" % len(movie_comments_time))
+                    # print("c vote: %d" % len(movie_comments_vote))
+                    # print("c text: %d" % len(movie_comments_text))
 
                 dataframe = pd.DataFrame({
                     "movie_id": movie_id,
@@ -114,6 +129,7 @@ class Spider:
                     "comment_vote": movie_comments_vote,
                     "comment_text": movie_comments_text
                 })
+                # 以$作为分隔符
                 dataframe.to_csv(self.filepath.format(path_list[ts], film_info[1]), encoding='utf-8', sep='$')
                 end_time = time.time()
                 print("-----Spider of film %s finish at %f s-----" % (film_info[0], end_time - start_time))
