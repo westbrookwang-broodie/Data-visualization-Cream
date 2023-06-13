159ba9018f966dd008e5c9ae7e0b9ca256a30f5b
wang <markwangyu@126,com>
2023-06-13 14:39:36 +0800

    + add rayMap

--- Tdesign/src/views/RayView.vue
+++ Tdesign/src/views/RayView.vue
@@ -1,5 +1,9 @@
 <template>
-  <div id="main1" style='width: 700px; height: 800px;'>
+  <t-select id="iu" placeholder="最受欢迎" v-model="type" autoWidth @change="icsv2(type)">
+    <t-option key="pop" label="最受欢迎" value="most_pop_filmray" />
+    <t-option key="score" label="评分最高" value="most_score_filmray" />
+  </t-select>
+  <div id="main1" style='width: 700px; height: 800px; margin-left: 00px'>
   </div>
 </template>
 
@@ -13,51 +17,177 @@ import math from "lodash";
 export default {
   name: "RayView",
   data() {
-    return {}
+    return {
+      type: '',
+      mychart: null,
+      option: null,
+      movie_name: [],
+      movies:[],
+    }
   },
-  mounted() {
+  methods: {
+    async icsv(name1) {
+      let this_ = this
+      await d3.csv('./src/data/' + name1 + '.csv',function (data1) {
+        this_.movie_name.push(data1.movie_title)
+        let tmp = {}
+        tmp.name = data1.movie_title
+        tmp.value = []
+        tmp.value.push(data1.movie_points)
+        tmp.value.push(data1.movie_long)
+        tmp.value.push(data1.movie_points_people)
+        tmp.value.push(data1.movie_comments_people)
+        tmp.value.push(data1.movie_want_to_see)
+        tmp.value.push(data1.movie_have_seen)
+        console.log(tmp)
+        this_.movies.push(tmp)
+      })
+      console.log(this.movies)
+      // for (let i=0;i<20;i++){
+      //   data[i] = data2[i].box;
+      //   name[i] = data2[i].name
+      // }
+    },
+    async icsv2(name1) {
+      let this_ = this
+      this.movie_name = []
+      this.movies = []
+      await d3.csv('./src/data/' + name1 + '.csv',function (data1) {
+        this_.movie_name.push(data1.movie_title)
+        let tmp = {}
+        tmp.name = data1.movie_title
+        tmp.value = []
+        tmp.value.push(data1.movie_points)
+        tmp.value.push(data1.movie_long)
+        tmp.value.push(data1.movie_points_people)
+        tmp.value.push(data1.movie_comments_people)
+        tmp.value.push(data1.movie_want_to_see)
+        tmp.value.push(data1.movie_have_seen)
+        console.log(tmp)
+        this_.movies.push(tmp)
+      })
+      console.log("type change")
+      console.log(this.movies)
+
+      let selec = {}
+      for (let i=0;i<20;i++){
+        selec[this.movie_name[i]] = false
+      }
+      var option
+
+      option = {
+        title: {
+          text: '电影雷达图',
+          style: {
+            marginTop: 300
+          }
+        },
+        legend: {
+          data: this.movie_name,
+          top: "5%",
+          selected: selec,
+        },
+        radar: {
+          // shape: 'circle',
+          indicator: [
+            { name: '电影评分', max: 10 },
+            { name: '电影时长', max: 180 },
+            { name: '电影评分人数', max: 2200000 },
+            { name: '电影评论人数', max: 500000 },
+            { name: '电影想看人数', max: 500000 },
+            { name: '电影看过人数', max: 4000000 }
+          ]
+        },
+        series: [
+          {
+            name: '电影雷达分布图',
+            type: 'radar',
+            data: this.movies,
+            top: "10%"
+            // selected: {
+            //   '霸王别姬': false
+            // }
+          },
+        ]
+      };
+      this.option = option
+      this.option && this.mychart.setOption(this.option);
+      // for (let i=0;i<20;i++){
+      //   data[i] = data2[i].box;
+      //   name[i] = data2[i].name
+      // }
+
+    }
+  },
+  async mounted() {
     var chartDom = document.getElementById('main1');
     var myChart = echarts.init(chartDom);
+    this.mychart = myChart
     var option;
+    await this.icsv('most_pop_filmray')
+    console.log('------------')
+    console.log(this.movie_name)
+
+    let selec = {}
+    for (let i=0;i<20;i++){
+      selec[this.movie_name[i]] = false
+    }
 
     option = {
       title: {
-        text: 'Basic Radar Chart'
+        text: '电影雷达图',
+        style: {
+          marginTop: 300
+        }
       },
       legend: {
-        data: ['Allocated Budget', 'Actual Spending']
+        data: this.movie_name,
+        top: "5%",
+        selected: selec,
       },
       radar: {
         // shape: 'circle',
         indicator: [
-          { name: 'Sales', max: 6500 },
-          { name: 'Administration', max: 16000 },
-          { name: 'Information Technology', max: 30000 },
-          { name: 'Customer Support', max: 38000 },
-          { name: 'Development', max: 52000 },
-          { name: 'Marketing', max: 25000 }
+          { name: '电影评分', max: 10 },
+          { name: '电影时长', max: 160 },
+          { name: '电影评分人数', max: 2200000 },
+          { name: '电影评论人数', max: 720000 },
+          { name: '电影想看人数', max: 900000 },
+          { name: '电影看过人数', max: 6000000 }
         ]
       },
       series: [
         {
-          name: 'Budget vs spending',
+          name: '电影雷达分布图',
           type: 'radar',
-          data: [
-            {
-              value: [4200, 3000, 20000, 35000, 50000, 18000],
-              name: 'Allocated Budget'
-            },
-            {
-              value: [5000, 14000, 28000, 26000, 42000, 21000],
-              name: 'Actual Spending'
-            }
-          ]
-        }
+          data: this.movies,
+          top: "10%"
+          // selected: {
+          //   '霸王别姬': false
+          // }
+        },
       ]
     };
+    this.option = option
     option && myChart.setOption(option);
   }
 }
+
+// async function icsv(name) {
+//   await d3.csv('./src/data/' + name + '.csv',function (data1) {
+//     data1.movie_vote_var *= 100
+//     data2.push(data1)
+//   })
+//   data2.sort((a, b)=>{
+//     return +b.movie_vote_var - +a.movie_vote_var
+//   })
+//   data2 = data2.slice(0,168)
+//   data2.sort(randomsort)
+//   // for (let i=0;i<20;i++){
+//   //   data[i] = data2[i].box;
+//   //   name[i] = data2[i].name
+//   // }
+// }
 </script>
 
 <style>
